24d43e3e10e5468dcac017d5958ffa68b24e17d0
trish181000 <trishna181000@gmail.com>
2023-12-05 08:36:51 -0700

    added button alert

--- file.js
+++ file.js
@@ -2,4 +2,9 @@
 const d = new Date();
 document.getElementById("date").innerHTML = d;
 
-// Footer Element:
+// Button Alert:
+function alert() {
+    document.getElementById("btn-alert").innerHTML = "I moved from Englad, July 2023, I took a one way flight and never went back!";
+  }
+
+// 
