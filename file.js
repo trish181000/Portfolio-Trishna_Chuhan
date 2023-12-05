403cacef3ca79efe7b05132b03399d1ae8b91c28
trish181000 <trishna181000@gmail.com>
2023-12-05 09:04:59 -0700

    Add Hover Button.

--- file.js
+++ file.js
@@ -4,7 +4,19 @@ document.getElementById("date").innerHTML = d;
 
 // Button Alert:
 function alert() {
-    document.getElementById("btn-alert").innerHTML = "I moved from Englad, July 2023, I took a one way flight and never went back!";
+    document.getElementById("btn-alert").innerHTML = "I moved from England, July 2023, I took a one way flight and never went back!";
   }
 
-// 
+// Hover Button:
+document.getElementById("hvr-btn").onmouseover = function() {mouseOver()};
+document.getElementById("hvr-btn").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("hvr-btn").innerHTML = "I am currently watching The Boys. :)";
+}
+
+function mouseOut() {
+  document.getElementById("hvr-btn").innerHTML = "Hover to find out what I am watching right now!";
+}
+
+// Incrementing Button:
